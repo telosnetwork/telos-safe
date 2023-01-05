57c6d983ff79ef0f12213bc70157b66b233bcf76
nick8319 <zhiltsov.nick@gmail.com>
2023-01-05 15:00:18 +0300

    feat: adding localStorage migration

--- src/services/ls-migration/addedSafes.ts
+++ src/services/ls-migration/addedSafes.ts
@@ -6,6 +6,8 @@ const IMMORTAL_PREFIX = '_immortal|v2_'
 
 const CHAIN_PREFIXES: Record<string, string> = {
   '1': 'MAINNET',
+  '40': 'TLOS',
+  '41': 'TLOS_TESTNET',
   '56': 'BSC',
   '100': 'XDAI',
   '137': 'POLYGON',
@@ -13,7 +15,7 @@ const CHAIN_PREFIXES: Record<string, string> = {
   '42161': 'ARBITRUM',
   '73799': 'VOLTA',
 }
-const ALL_CHAINS = ['1', '100', '137', '56', '246', '42161', '1313161554', '43114', '10', '5', '73799']
+const ALL_CHAINS = ['1', '40', '41', '100', '137', '56', '246', '42161', '1313161554', '43114', '10', '5', '73799']
 
 const OLD_LS_KEY = '__SAFES'
 
