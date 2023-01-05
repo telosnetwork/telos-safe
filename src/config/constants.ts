a68f301c10f9fc2ded19a292d1dfc1985a9e8b61
nick8319 <zhiltsov.nick@gmail.com>
2023-01-05 14:53:43 +0300

    feat: adding Telos support

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,9 +2,8 @@ import chains from './chains'
 
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
 
-export const GATEWAY_URL_PRODUCTION =
-  process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-client.safe.global'
-export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://safe-client.staging.5afe.dev'
+export const GATEWAY_URL_PRODUCTION = process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.safe.telos.net'
+export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.safe.telos.net'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
